f7f3d4bef082c3821fb87c21e38f631ec2c441a3
isaacguerreiro <isaacguerreirocom@gmail.com>
2021-10-16 18:52:18 -0300

    Entrypoint check if alert argument is passed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
-
 echo $1 $2 $3 $4 $5 $6 $7 $8
-eval "/synthesizable -i $1 -o $2 -r $3 -u $4 -p $5 -c $6 -s $7 -a $8"
+if [ -z  "$8" ] 
+then  
+  eval "/synthesizable -i $1 -o $2 -r $3 -u $4 -p $5 -c $6 -s $7"
+else
+  eval "/synthesizable -i $1 -o $2 -r $3 -u $4 -p $5 -c $6 -s $7 -a"
+fi
